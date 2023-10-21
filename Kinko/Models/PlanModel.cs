d211f04e0f9a198db07bcfd02b3e8971eefae6fa
Rob Ranf <rob@emiyaconsulting.com>
2023-10-20 19:24:54 -0700

    Add project files.

---
+++ Kinko/Models/PlanModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Kinko.Models
+{
+    public class PlanModel
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
